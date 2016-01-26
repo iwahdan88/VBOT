4f05961d5de815cad71aec5684ccbcafddfe361f
Islam Wahdan <iislam27@hotmail.com>
2016-01-26 02:22:48 +0200

    First Trial

--- Application.py
+++ Application.py
@@ -2,11 +2,11 @@
 
 ########################################################################################################################
 #                                                                                                                      #
-#       File Name:    Member.py                                                                                        #
+#       File Name:    Application.py                                                                                   #
 #                                                                                                                      #
 #       Author:       Islam Wahdan                                                                                     #
 #                                                                                                                      #
-#       Description:  Class Defining a Bus Member and all his/her attributes                                           #
+#       Description:  Application Handling User Interactions                                                           #
 #                                                                                                                      #
 #       Language:     Python                                                                                           #
 #                                                                                                                      #
@@ -20,78 +20,32 @@
 #       Initial Revision                                                                                               #
 # -------------------------------------------------------------------------------------------------------------------- #
 import Defs
+import Member
+import Database
+import telepot
+import json
 
-class BusMember:
-    """Class Defining a Typical Bus Member"""
+MyBot = telepot.Bot("102176437:AAFufVB0sWxKhJn34Mo8EmyOSJjeUb436WM")
 
-    def __init__(self, username, memtype, status, admin):
-        """
-        :type username: String
-        :type memtype: MembershipType
-        :type status: TripRegStatusject
-        :type admin: boolean
-        """
-        self.UserName = username
-        self.MemType = memtype
-        self.TripStatus = status
-        self.IsAdmin = admin
+def application(environ, start_response):
 
-    def GetUserName(self):
-        """
+    if environ['REQUEST_METHOD'] == 'POST':
 
-        :return: String
-        """
-        return self.UserName
+        ctype = 'text/html'
+        rawdata = environ['wsgi.input'].read()
+        DecaodedData = json.load(rawdata)
+        ChatID = DecaodedData['chat']
+        FirstName = DecaodedData['first_name']
+        Lastname = DecaodedData['last_name']
+        MessageID = DecaodedData['message_id']
 
-    def GetMemType(self):
-        """
+        MyBot.sendMessage(chat_id=ChatID, text="ACKNOLEDGE", reply_to_message_id=MessageID)
 
-        :return: Defs.MembershipType
-        """
-        return self.MemType
+    response_body = "OK"
+    response_body = response_body.encode('utf-8')
 
-    def GetTripStatus(self):
-        """
-
-        :return: Defs.TripRegStatus
-        """
-        return self.TripStatus
-
-    def GetAdminRights(self):
-        """
-
-        :return: Boolean
-        """
-        return self.IsAdmin
-
-    def SetUserName(self, user):
-        """
-
-        :param user: User name
-        :return: None
-        """
-        self.UserName = user
-
-    def SetMemType(self, memtype):
-        """
-
-        :param memtype: Membership Type
-        :return: None
-        """
-        self.MemType = memtype
-
-    def SetTripStatus(self, status):
-        """
-
-        :param status: Member Trip Status
-        :return: None
-        """
-        self.TripStatus = status
-
-    def SetAdmin(self, admin):
-        """
-
-        :param admin: Member Admin Rights
-        :return: None
-        """
-        self.IsAdmin = admin
+    status = '200 OK'
+    response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
+    #
+    start_response(status, response_headers)
+    return [response_body]
--- Database.py
+++ Database.py
@@ -20,18 +20,84 @@
 #       Initial Revision                                                                                               #
 # -------------------------------------------------------------------------------------------------------------------- #
 import Defs
-import io
-import xml
+import PyMySQL
 
 class MemDataBase:
     """ Class for Handling Members Data """
 
     # Active Working Members Buffer
     ActiveMembers = []
+    Admins = []
 
-    def SerilaizeMember(self, member):
+    def __init__(self):
+
+        self.DbConnection = PyMySQL.connect(host='localhost', user='adminfBZBGAJ',
+                                            password='9WhbQL6DcdlD', db='vbothome',
+                                            charset='utf8mb4', cursorclass=PyMySQL.cursors.DictCursor)
+
+    def __del__(self):
+        # Close Connection
+        self.DbConnection.close()
+
+    def AddToDB(self, member):
         """
-        :param member:
-        :return: bool
+        :param member: Member Object
+        :return: Boolean
         """
-        
+        try:
+            cursor = self.DbConnection.cursor()
+            # Create a new record
+            sql = "INSERT INTO `users` (`FullName`, `IsAdmin`) VALUES (%s, %s)"
+            cursor.execute(sql, (member.GetUserName(), member.GetAdminRights()))
+            # Execute
+            self.DbConnection.commit()
+        finally:
+            # Operation Unsuccessful
+            return False
+        # Operation Successful
+        return True
+
+    def RemoveFromDB(self, member):
+        """
+
+        :param member: Member Object
+        :return: Boolean
+        """
+        try:
+            cursor = self.DbConnection.cursor()
+            # Create a new record
+            sql = "DELETE FROM `bus_members` WHERE FullName = %s"
+            cursor.execute(sql, (member.GetUserName()))
+            # Execute
+            self.DbConnection.commit()
+        finally:
+            # Operation Unsuccessful
+            return False
+        # Operation Successful
+        return True
+    def SearchDB(self, member):
+        """
+
+        :param member: Member Object
+        :return: UCHAR
+        """
+        # Search in Working Buffer First
+        if member.GetUserName() in MemDataBase.ActiveMembers:
+            # Member Found
+            return 1
+        else:
+            # Search DB
+            try:
+                cursor = self.DbConnection.cursor()
+                # Create a new record
+                sql = "SELECT * FROM `bus_members` WHERE `FullName`=%s"
+                cursor.execute(sql, member.GetUserName())
+                result = cursor.fetchone()
+            finally:
+                # Operation Unsuccessful
+                return 255
+            if result:
+                return 1
+            else:
+                return 0
+
